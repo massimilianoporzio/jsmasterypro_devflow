bad986d30e7d50d5bd793c61265a6bcf9e6c3ae9
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2026-04-14 23:16:13 +0200

    tailwind setup escluso fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+/*
+ *   Copyright (c) 2026 Massimiliano Porzio
+ *   All rights reserved.
+ */
 const Page = () => {
   return (
     <div>
       {" "}
-      <h1 className="text-3xl">Welcome to the ultimate next.js course!</h1>
+      <h1 className="h1-bold">Welcome to the ultimate next.js course! 👏</h1>
     </div>
   );
 };
