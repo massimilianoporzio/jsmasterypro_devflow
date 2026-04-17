0c4083f63f8d34eb267516bff5343fb1fed659a2
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2026-04-17 23:49:22 +0200

    iniziato navlinks

---
+++ src/components/navigation/navbar/NavLinks.tsx
@@ -0,0 +1,10 @@
+/*
+ *   Copyright (c) 2026 Massimiliano Porzio
+ *   All rights reserved.
+ */
+
+const NavLinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
+  return <div>NavLinks</div>;
+};
+
+export default NavLinks;
--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -1,20 +1,35 @@
+/*
+ *   Copyright (c) 2026 Massimiliano Porzio
+ *   All rights reserved.
+ */
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import Theme from "./Theme";
+
+import MobileNavigation from "./MobileNavigation";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none">
-      <Link href="/" className="flex items-center gap-1">
-        <Image src="/images/site-logo.svg" alt="DevFlow Logo" width={23} height={23} className="object-contain" />
+    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 dark:shadow-none">
+      <Link
+        href="/"
+        className="flex items-center gap-1"
+      >
+        <Image
+          src="/images/site-logo.svg"
+          alt="DevFlow Logo"
+          width={23}
+          height={23}
+          className="object-contain"
+        />
         <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
           Dev<span className="text-primary-500">Flow</span>
         </p>
       </Link>
       <p>Global Search</p>
-      <div className="flex-between">
+      <div className="flex-between gap-5">
         <Theme />
+        <MobileNavigation />
       </div>
     </nav>
   );
