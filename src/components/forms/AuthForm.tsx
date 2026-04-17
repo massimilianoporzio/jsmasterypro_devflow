bd61bb86d0ce37a2c87db593b8869e64f930cda3
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2026-04-17 19:46:14 +0200

    correzione per apostrofo

--- src/components/forms/AuthForm.tsx
+++ src/components/forms/AuthForm.tsx
@@ -70,7 +70,7 @@ const AuthForm = <T extends FieldValues>({ schema, defaultValues, onSubmit, form
       </FieldGroup>
       {formType === "SIGN_IN" ? (
         <p>
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <Link href={ROUTES.SIGN_UP} className="paragraph-semibold primary-text-gradient">
             Sign Up
           </Link>
